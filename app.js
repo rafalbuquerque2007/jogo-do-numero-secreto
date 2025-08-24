0881c6b8c496f2397949187e08325b22b920979a
Rafael <rafalbuquerque2007@gmail.com>
2025-08-23 23:40:25 -0300

    mudanças no numero secreto

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativa = 1;
 
@@ -12,7 +12,7 @@ function exibirMensagemInicial(){
 
         exibirTextoNaTela("h1", "Jogo do número secreto nova versão");
 
-        exibirTextoNaTela("p", "Escolha um número entre 1 e 10");        
+        exibirTextoNaTela("p", "Escolha um número entre 1 e 100");        
 }
 
 exibirMensagemInicial();
