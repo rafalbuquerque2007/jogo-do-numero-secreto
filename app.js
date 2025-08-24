6529e9bcc8b1a6c7e125c71f36305b83fac80853
rafalbuquerque2007 <rafalbuquerque2007@gmail.com>
2025-08-23 21:15:57 -0300

    Novo nome

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto){
         responsiveVoice.speak(texto,"Brazilian Portuguese Female", {rate:1.2});
 }
 function exibirMensagemInicial(){
-        exibirTextoNaTela("h1", "Jogo do número secreto");
+        exibirTextoNaTela("h1", "Jogo do número secreto nova versão");
         exibirTextoNaTela("p", "Escolha um número entre 1 e 10");        
 }
 
